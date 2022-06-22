d52545f4ab14177aa035ae9a0ce9d15df21474b4
Thiago Henrique PC <tiagohenrriquebuenodeOliveira@gmail.com>
2022-06-22 10:28:36 -0300

    Header e logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { Header } from "./components/Header"
-import { Lesson } from "./components/Lesson"
-import { Sidebar } from "./components/Sidebar"
-import { Video } from "./components/Video"
-import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
+
 
 function App() {
   return (
     <div>
-      <Header />
-      <Sidebar />
-      <Video />
-      <Lesson />
+      <Event />
     </div>
   )
 }
